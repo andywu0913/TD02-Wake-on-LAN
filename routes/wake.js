c4c49a35d291d76c48fd8e9bcbf97ef00e439f74
Andy Wu <andywu0913@yahoo.com.tw>
2021-05-16 09:35:51 +0800

    Finish wake function.

--- routes/wake.js
+++ routes/wake.js
@@ -1,17 +1,54 @@
 const express = require('express');
-const wolDao = require('../dao/wolDao');
+const util = require('util');
+const wol = require('wol');
+const addressDao = require('@dao/addressDao');
 
 const router = express.Router();
+const wakeOnLAN = util.promisify(wol.wake);
 
-router.post('/:id', (req, res) => {
-  const mac = '18:C0:4D:4C:93:12';
-  wolDao.wake(mac, { address: '192.168.11.255' }, (err) => {
-    if (err) {
-      throw err;
+const LAN_BROADCAST_ADDRESS = '192.168.11.255';
+
+router.post('/all', async (req, res) => {
+  try {
+    const addressArr = await addressDao.query();
+    console.log(addressArr);
+
+    const result = await Promise.all(
+      addressArr.map((addressObj) => wakeOnLAN(addressObj.macAddress, { address: LAN_BROADCAST_ADDRESS }))
+    );
+
+    console.log({result});
+
+    return res.send('已發送開機訊號至所有設備');
+  } catch (error) {
+    console.error(error);
+    return res.status(500).send(`發送開機訊號至所有設備失敗: ${error}`);
+  }
+});
+
+router.post('/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const [result] = await addressDao.query(id);
+
+    if (!result) {
+      return res.status(400).send('ID錯誤');
     }
-    console.log('send magic packet to %s success.', mac);
-  });
-  res.send();
+
+    const { macAddress } = result;
+
+    const isSuccess = await wakeOnLAN(macAddress, { address: LAN_BROADCAST_ADDRESS });
+
+    if (!isSuccess) {
+      return res.status(500).send(`發送開機訊號失敗: ${error}`);
+    }
+
+    return res.send(`已發送開機訊號至${result.name}`);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).send(`發送開機訊號失敗: ${error}`);
+  }
 });
 
 module.exports = router;
